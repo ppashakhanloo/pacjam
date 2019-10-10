4bf2da759854da79285b130e32b6b50de7d590fc
root <root@b4c5e7816551>
2019-10-10 20:51:38 +0000

    add the use-ratio to dep graphs

--- debtree-scripts/color-dependencies.sh
+++ debtree-scripts/color-dependencies.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-./debtree --arch=amd64 --no-alternatives --no-provides --no-conflicts --no-versions "$1" > a.dot
+./debtree --arch=amd64 --no-recommends --no-alternatives --no-provides --no-conflicts --no-versions "$1" > a.dot
 
-grep -o '".*"' a.dot | sed 's/->/\n/g'  | sed 's/"//g' | sed 's/\[.*//' | sed 's/\:.*//' | sed 's/[ \t]*$//' | sed 's/^[ \t]*//' | sort -u > package-list
+grep -o '".*"' a.dot | grep '[-][>]' | sed 's/->/\n/g'  | sed 's/"//g' | sed 's/\[.*//' | sed 's/\:.*//' | sed 's/[ \t]*$//' | sed 's/^[ \t]*//' | sort -u > package-list
+
+sed -i 's/Pr_//g' package-list
+sed -i '/^ *$/d' package-list
 
 sed 's/$/,0/g' package-list > package-list-versioned
+
 
 first=$(awk '/->/ { ln = FNR } END { print ln }' a.dot)
 first=$((first+1))
@@ -30,6 +34,14 @@ while read line; do
   cves_total=$((cves_total+line))
 done < cves
 
+declare -A popcon_inst
+declare -A popcon_vote
+while IFS=, read -r package inst vote
+do
+  popcon_inst[$package]=$inst
+  popcon_vote[$package]=$vote
+done < ALL-CVES
+
 package_list=()
 while read line; do
   echo "$line"
@@ -41,8 +53,27 @@ for package in "${package_list[@]}"
 do
   curr_cves="${cve_list[$index]}"
   index=$((index+1))
+  echo "$package"
+  echo "${popcon_inst[$package]}"
+  echo "${popcon_vote[$package]}"
+  
   color=`echo "$curr_cves $cves_total" | python3 color.py`
+  
+  dummy_inst=${popcon_inst[$package]}
+  dummy_vote=${popcon_vote[$package]}
+  if [ ! ${popcon_inst[$package]} ] ; then
+    dummy_inst=-1
+  fi
+
+  if [ ! ${popcon_vote[$package]} ] ; then
+    dummy_vote=-1
+  fi
+  
+  border=`echo "$dummy_inst $dummy_vote" | python3 border.py`
   pos=$((first+j))
-  sed -e "${pos}i\"$package\" [style=filled,fillcolor=\"$color\"];" b.dot > c.dot
+  sed -e "${pos}i\"$package\" [style=filled,fillcolor=\"$color\",peripheries=\"$border\",shape="oval"];" b.dot > c.dot
   mv c.dot b.dot
 done
+
+# draw the dependency graph
+dot -Tpng -o images/"$1".png b.dot 
