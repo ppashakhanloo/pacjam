6bf310b03e2e10664cb921ecc39378e31d6517ad
Kihong Heo <kheo@cis.upenn.edu>
2019-10-16 23:38:33 -0400

    [Policy] fix bnet generator

--- policy/bnet-generator.py
+++ policy/bnet-generator.py
@@ -85,7 +85,6 @@ def store_vertex_dict(args, g, name2idx, result):
     with open('{}.fg'.format(args.package), 'w') as f:
         f.write('{}\n\n'.format(len(factors)))
         for pkg, factor in factors.items():
-            print(pkg)
             print_factor(g, factor, f)
 
 
@@ -135,37 +134,45 @@ def match_all(unreachable_daily_info_list, date):
 def compute_conditional_probability(target, target_daily_info,
                                     reachable_daily_info_list,
                                     unreachable_daily_info_list, bin_vector):
+    # compute probability of even number of binary vector
+    # Note:
+    # - 0th element of bin_vector is conclusion
+    # - 1st element of bin_vector is unreachable (if unreachable exists)
+    # - the other elements are hyphotheses (n, n-1, ..., 0th element of reachable) (if exists)
     hypothesis_date = 0
     conclusion_date = 0
+
     for date in target_daily_info[target]:
         # count the number of days where the hypothesis matches
         hypothesis_hit = True
         i = 0
         for pred, reachable_daily_info in reachable_daily_info_list.items():
             hypothesis_hit = hypothesis_hit and match(
-                reachable_daily_info[pred], date, bin_vector[i])
+                reachable_daily_info[pred], date, bin_vector[-i - 1])
             i += 1
+
         if hypothesis_hit:
             # if there is no unreachable pred, hit
             if len(unreachable_daily_info_list) == 0:
                 hypothesis_date += 1
             # if there exists at least one use of unreachable node then it is used
-            elif bool(int(bin_vector[-2])) and match_exists(
+            elif bool(int(bin_vector[1])) and match_exists(
                     unreachable_daily_info_list, date):
                 hypothesis_date += 1
             # if all unreachable nodes do not use then hit then it is not used
-            elif not bool(int(bin_vector[-2])) and match_all(
+            elif not bool(int(bin_vector[1])) and match_all(
                     unreachable_daily_info_list, date):
                 hypothesis_date += 1
             else:
                 hypothesis_hit = False
 
         # count the number of days where the conclusion matches
-        conclusion_hit = match(target_daily_info[target], date, bin_vector[-1])
+        conclusion_hit = match(target_daily_info[target], date, bin_vector[0])
         if hypothesis_hit and conclusion_hit:
             conclusion_date += 1
+    # if there is no maching data, assign 0.5
     if hypothesis_date == 0:
-        return 0
+        return 0.5
     else:
         return float(conclusion_date) / float(hypothesis_date)
 
@@ -179,10 +186,10 @@ def print_factor(g, factor, f):
     # node ids
     s = ""
     for node in factor['reachable']:
-        s = s + str(id_dict[node]) + " "
+        s = str(id_dict[node]) + " " + s
     if factor['unreachable_id'] is not None:
-        s = s + str(factor['unreachable_id']) + " "
-    s = s + str(factor['id'])
+        s = str(factor['unreachable_id']) + " " + s
+    s = str(factor['id']) + " " + s
     f.write(s + "\n")
     # arity
     s = ""
@@ -228,16 +235,41 @@ def daily_information(preds):
 def generate_factor_table(name, target_daily_info, reachable_daily_info_list,
                           unreachable_daily_info_list):
     factor_table = []
-    # consider len(reachable) + 2 variables. one is for the target, the other is an aggregation of all unreachable
-    bitvector_length = len(reachable_daily_info_list) + int(
-        unreachable_daily_info_list != []) + 1
-    for i in range(0, int(math.pow(2, bitvector_length))):
-        bin_vector = list('{}'.format(get_bin(i, bitvector_length)))
+
+    if len(reachable_daily_info_list) + len(unreachable_daily_info_list) == 0:
+        # if it is dummy node, only 1 bit
+        bitvector_length = 1
+    else:
+        # if not dummy node, consider len(reachable) + 2 variables.
+        # one is for the target, the other is an aggregation of all unreachable
+        bitvector_length = len(reachable_daily_info_list) + int(
+            unreachable_daily_info_list != []) + 1
+    for i in range(0, int(math.pow(2, bitvector_length)), 2):
+        bin_vector = list(reversed('{}'.format(get_bin(i, bitvector_length))))
         cond_prob = compute_conditional_probability(
             name, target_daily_info, reachable_daily_info_list,
             unreachable_daily_info_list, bin_vector)
         factor_table.append(cond_prob)
+        factor_table.append(1.0 - cond_prob)
     return factor_table
+
+
+def merge_daily_info(name, daily_info_list):
+    all_keys = set()
+    for pkg, info in daily_info_list.items():
+        all_keys.update(set(info[pkg].keys()))
+    merged = {}
+    for date in all_keys:
+        binary = False
+        vote = 0
+        for pkg, info in daily_info_list.items():
+            # if there exists at least one use, then it is used
+            if date in info[pkg]:
+                binary = binary or info[pkg][date]['binary']
+                vote = vote + info[pkg][date]['vote']
+        merged[date] = {'vote': vote, 'binary': binary}
+    info[name] = merged
+    return info
 
 
 def generate_factor_graph(args, g, reachable_nodes):
@@ -245,7 +277,7 @@ def generate_factor_graph(args, g, reachable_nodes):
     total = len(reachable_nodes)
     count = 0
     result = {}
-    result['skipped'] = 0
+    result['pruned'] = 0
     factors = {}
     for pkg in reachable_nodes:
         count += 1
@@ -261,7 +293,7 @@ def generate_factor_graph(args, g, reachable_nodes):
             vp, pkg, reachable_nodes)
 
         if len(reachable_preds) > args.max_pred:
-            result['skipped'] += 1
+            result['pruned'] += 1
             logging.warn(
                 'Prune preds of {} with too many preds ({} / {} are reachable)'.
                 format(name, len(reachable_preds), pkg.in_degree()))
@@ -280,13 +312,19 @@ def generate_factor_graph(args, g, reachable_nodes):
             uid = get_new_id()
             uname = 'dummy{}'.format(uid)
             store_id_dict(uname, uid)
+            merged_daily_info = merge_daily_info(uname,
+                                                 unreachable_daily_info_list)
             factors[uname] = {
                 'reachable': [],
                 'unreachable': [],
-                # TODO
-                'factor_table': [0.5, 0.5],
-                'id': uid,
-                'unreachable_id': None
+                'factor_table':
+                generate_factor_table(uname, merged_daily_info, {}, {}),
+                'id':
+                uid,
+                'unreachable_id':
+                None,
+                'name':
+                uname
             }
         else:
             uid = None
@@ -298,7 +336,8 @@ def generate_factor_graph(args, g, reachable_nodes):
             'unreachable': unreachable_daily_info_list,
             'factor_table': factor_table,
             'id': nid,
-            'unreachable_id': uid
+            'unreachable_id': uid,
+            'name': name
         }
         factors[name] = info
 
@@ -340,8 +379,8 @@ def main():
     result = generate_factor_graph(args, g, reachable_nodes)
     store_vertex_dict(args, g, name2idx, result)
 
-    logging.info('Skipped nodes: {}'.format(result['skipped']))
-    print('\nDone ({}sec)'.format(int(time.process_time() - start)))
+    logging.info('# nodes with pruned preds: {}'.format(result['pruned']))
+    print('\nDone ({} sec)'.format(int(time.process_time() - start)))
 
 
 if __name__ == '__main__':
