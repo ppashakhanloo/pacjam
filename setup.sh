bf1fc1e9f93d70b1411657ec0297676b06e23976
Anthony Canino <anthony.canino1@gmail.com>
2019-10-31 22:24:40 +0000

    Install script.

--- setup.sh
+++ setup.sh
@@ -5,8 +5,21 @@ apt-get install -y debhelper
 apt-get install -y bear
 
 # Hooking into build system
-sudo mv /usr/bin/make /usr/bin/make-orig
-cp aux/make /usr/bin/
+if [ ! -f /usr/bin/make-orig ]; then
+  sudo mv /usr/bin/make /usr/bin/make-orig
+  cp aux/make /usr/bin/
+fi
+
+if [ ! -f /usr/bin/gcc-orig ]; then
+  sudo mv /usr/bin/gcc /usr/bin/gcc-orig
+  cp aux/gcc /usr/bin/
+fi
+
+if [ ! -f /usr/bin/g++-orig ]; then
+  sudo mv /usr/bin/g++ /usr/bin/g++-orig
+  cp aux/g++ /usr/bin/
+fi 
+
 cp aux/buildflags.conf /etc/dpkg/
 
 # Setup for lzload
