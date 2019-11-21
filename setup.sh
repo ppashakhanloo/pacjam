29384b45d4f55d109a33567dfcb068ac012a691b
Kihong Heo <kihong.heo@gmail.com>
2019-11-21 23:48:16 +0000

    fix sudo isssue

--- setup.sh
+++ setup.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
 # Dependencies
-apt-get install -y debhelper
-apt-get install -y bear
+sudo apt-get install -y debhelper
+sudo apt-get install -y bear
 
 # Hooking into build system
 if [ ! -f /usr/bin/make-orig ]; then
   sudo mv /usr/bin/make /usr/bin/make-orig
 fi
-cp aux/make /usr/bin/
+sudo cp aux/make /usr/bin/
 
 if [ ! -f /usr/bin/gcc-orig ]; then
   sudo mv /usr/bin/gcc /usr/bin/gcc-orig
 fi
-cp aux/gcc /usr/bin/
+sudo cp aux/gcc /usr/bin/
 
 if [ ! -f /usr/bin/g++-orig ]; then
   sudo mv /usr/bin/g++ /usr/bin/g++-orig
 fi 
-cp aux/g++ /usr/bin/
+sudo cp aux/g++ /usr/bin/
 
 if [ ! -f /usr/bin/ld-orig ]; then
   sudo mv /usr/bin/ld /usr/bin/ld-orig
 fi 
-cp aux/ld /usr/bin/
+sudo cp aux/ld /usr/bin/
 
 # Setup for lzload
 LZLOAD_PATH=$HOME/var/lib/lzload
