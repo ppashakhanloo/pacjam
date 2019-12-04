5bedda8c7848bf395488c42b128b63b669294008
Kihong Heo <kihong.heo@gmail.com>
2019-12-04 16:37:20 +0000

    update dep-find-new

--- dep-find-new.py
+++ dep-find-new.py
@@ -1,13 +1,58 @@
 #!/usr/bin/python3
 
+import subprocess
 import json
-import sys
+import os.path
 
-with open('deps.jessie.json') as f:
-    db = json.load(f)
+from optparse import OptionParser 
 
 
-def fixpt(works, deps):
+def load(filename):
+    if os.path.exists(filename):
+        with open(filename, 'r') as f:
+            db = json.load(f)
+        return db, False
+    else:
+        return {}, True
+
+
+def save(filename, deps):
+    with open(filename, 'w') as f:
+        json.dump(deps, f, indent=2)
+
+
+def fetch(filename, deps):
+    lst = subprocess.check_output(['apt-cache', 'search', '.']).decode()
+    counter = 0
+    lst = lst.split('\n')
+    total = len(lst)
+    counter = 0
+    for pkg in lst:
+        name = pkg.split(' ')[0]
+        counter += 1
+        print('[{}/{}] Fetching {}'.format(counter, total, name))
+        if name in deps:
+            continue
+        if name == '':
+            continue
+        depends = subprocess.check_output([
+            'apt-cache', 'depends', '--no-suggests', '--no-breaks',
+            '--no-conflicts', name
+        ]).decode()
+        dep_list = []
+
+        for dep in [dep for dep in depends.split('\n') if 'Depends:' in dep or 'Recommends:' in dep]:
+            dep_name = dep.split(':')[1].strip()
+            dep_list.append(dep_name)
+        deps[name] = dep_list
+
+        if counter % 1000 == 0:
+            save(filename, deps)
+    save(filename, deps)
+    return deps
+
+
+def fixpt(db, works, deps):
     if len(works) == 0:
         return deps
     w = works.pop()
@@ -23,12 +68,22 @@ def fixpt(works, deps):
             deps.update(directs)
     return fixpt(works, deps)
 
-target = sys.argv[1]
+
+parser = OptionParser()
+parser.add_option('-p', '--package', dest='package', help='build a dependency list for PACKAGE', metavar='PACKAGE')
+parser.add_option('-o', '--output', dest='output', help='output json file', metavar='OUPTUT')
+
+(options, args) = parser.parse_args()
+
+db, need_init = load(options.output)
+if need_init:
+    db = fetch(options.output, db)
+
 works = set()
-works.add(target)
-deps = set()
-deps.add(target)
-trans_deps = fixpt(works, deps)
+works.add(options.package)
+results = set()
+results.add(options.package)
+trans_deps = fixpt(db, works, results)
 
 for l in trans_deps:
     if '<' not in l:
